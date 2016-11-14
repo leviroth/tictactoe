15a1b35c79b564a9acca4ee2b6b653758db812a7
Levi Roth <levimroth@gmail.com>
2016-11-14 12:48:05 -0500

    Major revision of win-checking

--- tictactoe.js
+++ tictactoe.js
@@ -3,7 +3,7 @@ var Player = function(piece, board) {
     this.board = board;
 
     this.play = function (x, y) {
-        if (this.board[x][y] == null) {
+        if (this.board[x][y] === null) {
             this.board[x][y] = this.piece;
             return true;
         }
@@ -18,6 +18,7 @@ var Game = function () {
     this.players = [new Player("X", this.board), new Player("O", this.board)];
     this.turn = 0;
     this.won = false;
+    this.GAMESIZE = 3;
 
     this.changeTurn = function () {
         this.turn ^= 1;
@@ -41,58 +42,34 @@ var Game = function () {
         return false;
     };
 
-    this.checkWin = function (currentPiece) {
-        for (var i = 0; i < 3; i++) {
-            if (this.checkRow(currentPiece, i)
-                || this.checkColumn(currentPiece, i)) {
+    this.possibleWin = function (startRow, startCol, stepRow, stepCol) {
+        var row = startRow;
+        var col = startCol;
+        var squares = [];
+        for (var i = 0; i < this.GAMESIZE; i++) {
+            squares.push(this.board[row][col]);
+            row += stepRow;
+            col += stepCol;
+        }
+        return squares;
+    };
+
+    this.checkPossible = function (squares, piece) {
+        return squares.every(function (square) {
+            return square === piece;
+        });
+    };
+
+    this.checkWin = function (piece) {
+        for (var i = 0; i < this.GAMESIZE; i++) {
+            var row = this.possibleWin(i, 0, 0, 1);
+            var col = this.possibleWin(0, i, 1, 0);
+            if (this.checkPossible(row, piece) || this.checkPossible(col, piece)) {
                 return true;
             }
         }
-        if (this.checkDiagonals(currentPiece)) {
-            return true;
-        }
-        return false;
+        var diag1 = this.possibleWin(0, 0, 1, 1);
+        var diag2 = this.possibleWin(0, this.GAMESIZE - 1, 1, -1);
+        return (this.checkPossible(diag1, piece) || this.checkPossible(diag2, piece));
     };
-
-    this.checkRow = function (currentPiece, row) {
-        for (var i = 0; i < 3; i++) {
-            if (this.board[row][i] != currentPiece) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    this.checkColumn = function (currentPiece, column) {
-        for (var i = 0; i < 3; i++) {
-            if (this.board[i][column] != currentPiece) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    this.checkDiagonals = function (currentPiece) {
-        return (this.checkDiagonal1(currentPiece)
-                || this.checkDiagonal2(currentPiece));
-    };
-
-    this.checkDiagonal1 = function (currentPiece) {
-        for (var i = 0; i < 3; i++) {
-            if (this.board[i][i] != currentPiece) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    this.checkDiagonal2 = function (currentPiece) {
-        for (i = 0; i < 3; i++) {
-            if (this.board[i][2 - i] != currentPiece) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-}
+};
